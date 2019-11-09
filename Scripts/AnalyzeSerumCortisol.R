184f6d79f5791f0c4d68b2417958ebfdef26f893
Charmaine <56736747+chipoli1@users.noreply.github.com>
2019-11-08 20:05:17 -0500

    Update AnalyzeSerumCortisol.R

--- Scripts/AnalyzeSerumCortisol.R
+++ Scripts/AnalyzeSerumCortisol.R
@@ -1,120 +1,75 @@
-## Install necessary packages
+# BTEC330 PROJECT 2
+## Submitted by Charmaine Hipolito
+
+# Install necessary packages
 
 install.packages("ggplot2")
 library(ggplot2)
+df<-na.omit(IBS)
 
-install.packages("scatterplot3d")
-library(scatterplot3d)
+# Read data
+IBS <- read.csv("Data/RobinsonEtAl_Sup1.csv", header = TRUE)
+head(IBS)
+write.csv(IBS, "Data_Output/output.csv")
 
-## Read data
-IBS1 <- read.csv("RobinsonEtAl_Sup1.csv", header = TRUE)
+IBS$SerumCortisol_result <- "NA"
 
-##  Single Regressions for BMI vs. each blood data variable
-##  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
-##  https://statquest.org/2017/10/30/statquest-multiple-regression-in-r/
-##  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
-##  http://r-statistics.co/Linear-Regression.html
+# Assign "HIGH", "NORMAL", or "LOW" based on clinical range to the SerumCortisol_result parameter
+###### Range was obtained from https://emedicine.medscape.com/article/2088826-overview
+
+IBS$SerumCortisol_result[IBS$SerumCortisol > 28] <- "HIGH"
+
+IBS$SerumCortisol_result[IBS$SerumCortisol <= 28 & IBS$SerumCortisol >= 7] <- "NORMAL"
+
+IBS$SerumCortisol_result[IBS$SerumCortisol < 7] <- "LOW"
 
 
-single.regression <- lm(BMI ~ SerumCortisol, data=IBS1)
-summary(single.regression)
+#  Single Regressions for BMI vs. SerumCortisol
+######  Data was obtained from Robinson, et al. 2019 (doi: https://doi.org/10.1101/608208)
+######  http://www.sthda.com/english/articles/40-regression-analysis/167-simple-linear-regression-in-r/
+######  http://r-statistics.co/Linear-Regression.html
 
-single.regression <- lm(BMI ~ CRP, data=IBS1)
-summary(single.regression)
+# Single Regression Test, BMI vs. SerumCortisol parameter
+SerumCortisol.regression <- lm(BMI ~ SerumCortisol, data=IBS)
+summary(SerumCortisol.regression)
 
-single.regression <- lm(BMI ~ ESR, data=IBS1)
-summary(single.regression)
+# Output the results to a file
+sink('Data_Output/SerumCortisol1.txt', append = TRUE)
+print(SerumCortisol.regression)
+sink()
 
-single.regression <- lm(BMI ~ PlateletCount, data=IBS1)
-summary(single.regression)
 
-single.regression <- lm(BMI ~ IgA, data=IBS1)
-summary(single.regression)
-
-single.regression <- lm(BMI ~ Lymphocytes, data=IBS1)
-summary(single.regression)
-
-## Multiple Regressions
-
-multiple.regression <- lm(BMI ~ SerumCortisol + CRP, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + ESR, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + ESR, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + PlateletCount, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + ACTH, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Lymphocytes, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgA, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Monocytes_PCT, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Monocytes, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Lymphocytes_PCT, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgG, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgE, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IgM, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Neutrophils, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + Neutrophil_PCT, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + IL10, data=IBS1)
-summary(multiple.regression)
-
-multiple.regression <- lm(BMI ~ SerumCortisol + LBP, data=IBS1)
-summary(multiple.regression)
-
-##Select the best 3-variable regression model based on highest scores from previous tests
-
-fit1 <- lm(BMI ~ SerumCortisol + CRP + ESR + PlateletCount + Lymphocytes , data=IBS1)
-summary(fit1)
-
-fit1 <- lm(BMI ~ SerumCortisol + CRP + ESR, data=IBS1)
-summary(fit1)
-
-fit1 <- lm(BMI ~ SerumCortisol + CRP, data=IBS1)
-summary(fit1)
-print(fit1)
+# ANOVA: IBS-subtype vs. SerumCortisol parameter
+###### http://www.sthda.com/english/wiki/one-way-anova-test-in-r
+SerumCortisol.aov <- aov(SerumCortisol ~ IBS.subtype, data=IBS)
+summary(SerumCortisol.aov)
+sink('Data_Output/SerumCortisol1.txt', append = TRUE)
+print(SerumCortisol.aov)
+sink()
 
 ## Scatterplots
-## https://www.statmethods.net/graphs/scatterplot.html
+###### https://www.statmethods.net/graphs/scatterplot.html
 
 
 ggplot(IBS1, aes(x=BMI, y=SerumCortisol)) +
   geom_point() +    
   geom_smooth(method=lm) 
 
-ggplot(IBS1, aes(x=BMI, y=CRP)) +
-  geom_point() +    
-  geom_smooth(method=lm)  
+png("Fig_Output/Rplot.png")
+SerumCortisol_scatterplot <- ggplot(IBS, aes(x = BMI, y = SerumCortisol)) +
+  geom_point() +
+  geom_smooth(method = lm)
+
+print(SerumCortisol_scatterplot)
+dev.off()
 
 
-## 3D scatterplot for the most significant 3-variable multiple regression model
-## http://www.sthda.com/english/wiki/scatterplot3d-3d-graphics-r-software-and-data-visualization
+# Boxplots
+###### https://www.statmethods.net/graphs/boxplot.html
+SerumCortisol_boxplot <- boxplot(SerumCortisol ~ IBS.subtype, data = IBS, main="SerumCortisol by IBS subtype",
+        xlab = "IBS.subtype", ylab = "SerumCortisol"
+        )
+print(SerumCortisol_boxplot)
+dev.off()
 
-s3d <- scatterplot3d(IBS$BMI, IBS$SerumCortisol, IBS$CRP,  pch=16, color="steelblue", box="TRUE", highlight.3d=FALSE, type="h", main="BMI x Cortisol x CRP")
-fit <- lm(SerumCortisol ~ BMI + CRP, data=IBS)
-s3d$plane3d(fit)
+
